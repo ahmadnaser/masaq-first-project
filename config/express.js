a3749a5eda864e043c500a4a296d791b05f702fb
Ahmad Naser <ahmadnassr@gmail.com>
2023-09-26 22:38:06 +0300

    mongoose

--- config/express.js
+++ config/express.js
@@ -1,10 +1,13 @@
 const config = require('./index')
 const express = require('express')
+const bp = require('body-parser')
+const formData = require('express-form-data')
 
 function setExpress(app) {
 
 
-
+    app.use(bp.json())
+    app.use(formData.parse())
 
     //views
     app.set('view engine', 'jsx');
