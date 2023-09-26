42374f6f773875a1aa5bd422f6899cffff3dc84e
Ahmad Naser <ahmadnassr@gmail.com>
2023-09-26 22:31:51 +0300

    Revert "mongoose basics and form data"
    This reverts commit 283745edc036f003246608ac69d62bee9b3d1a5a.

--- controller/homeController.js
+++ controller/homeController.js
@@ -5,54 +5,63 @@ const pets = require("../data/pets.json");
 
 //default route ~ default actoin ~ home ~ main landing page
 router.get("/", (req, res) => {
-  res.status(200);
-  // props.data
-  console.log(pets);
-  res.render("home", { data: pets });
+    res.status(200);
+    // props.data
+    console.log(pets);
+    res.render("home", { data: pets });
 });
 
 router.get("/addpet", (req, res) => {
-  res.status(200);
-  res.render("addPet");
-});
+    res.status(200)
+    res.render("addPet")
+})
+
 router.post("/AddPet", (req, res) => {
-  res.status(200);
-  res.render("addPet");
-});
+    console.log(req.body)
+    res.status(200)
+
+    if (req.body.name) {
+        //save in db
+        res.render("addPet", { message: "Successfully posted new pet!" })
+    } else {
+        res.render("addPet")
+    }
+
+})
 
 router.get("/about", (req, res) => {
-  console.log(req.body);
-  res.status(200);
-  res.render("addPet");
+    console.log(req.body);
+    res.status(200);
+    res.render("addPet");
 });
 
 router.get("/home", (req, res) => {
-  res.status(302);
-  res.redirect("/");
+    res.status(302);
+    res.redirect("/");
 });
 
 router.get("/welcome", (req, res) => {
-  res.status(200);
-  res.send("Welcome Masaq");
+    res.status(200);
+    res.send("Welcome Masaq");
 });
 router.get("/hello", (req, res) => {
-  res.status(200);
-  res.send("Hello in Masaq");
+    res.status(200);
+    res.send("Hello in Masaq");
 });
 
 router.get("/hello/:firstname/:lastname", (req, res) => {
-  res.status(200);
-  res.send(`Welcome ${req.params.firstname} ${req.params.lastname}`);
+    res.status(200);
+    res.send(`Welcome ${req.params.firstname} ${req.params.lastname}`);
 });
 
 router.get("/welcome", (req, res) => {
-  res.status(200);
-  res.send("Welcome to www.Masaq.it");
+    res.status(200);
+    res.send("Welcome to www.Masaq.it");
 });
 
 router.get("/pets", (req, res) => {
-  res.status(200);
-  res.json(pets);
+    res.status(200);
+    res.json(pets);
 });
 
 module.exports = router;
